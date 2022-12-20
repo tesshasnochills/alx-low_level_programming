53b86598c9259bd2c0f4cb6790a160fb5fb61072
tesshasnochills <tess.nganga2020@gmail.com>
2022-12-20 20:48:25 +0300

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: lenth of string
+ */
+int _strlen(char *s)
+{
+  int i;
+
+  i = 0;
+
+  while (s[i] != '\0')
+  {
+    i++;
+  }
+  return (i);
+}
