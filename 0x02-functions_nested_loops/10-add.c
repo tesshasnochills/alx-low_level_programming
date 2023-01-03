6bab569412f5240e9ae980c0ea5725f4db697802
tesshasnochills <tess.nganga2020@gmail.com>
2023-01-03 22:30:11 +0300

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer 
+ * @b: first integer
+ *
+ * Return: the value of the sum of @i and @j
+ */
+int add(int a, int b)
+{
+  return(a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with zero
+ */
+void times_table(void)
+{
+
+}
