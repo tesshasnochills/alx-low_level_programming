7ef2a13c9812047515934159ccb37a32a1f4050e
tesshasnochills <tess.nganga2020@gmail.com>
2023-01-23 16:32:49 +0300

    correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,8 @@ char *_strncpy(char *dest, char *src, int n)
 	for (t = 0; (t < n && *(src + t) != '\0'); t++)
 		*(dest + t) = *(src + t);
 
-	for (t = 0; t < n; t++)
+	for (; t < n; t++)
 		*(dest + t) = '\0';
+
 	return (dest);
 }
