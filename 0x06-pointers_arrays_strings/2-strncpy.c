237c8c6b30e25bcc5a35c3bc667a498751591194
tesshasnochills <tess.nganga2020@gmail.com>
2023-01-23 16:30:33 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strncpy - copies a string
+* @dest: buffer to copy string to
+* @src: string to copy
+* @n: most number of bytes copied to dest
+*
+* Return: @dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int t;
+
+	for (t = 0; (t < n && *(src + t) != '\0'); t++)
+		*(dest + t) = *(src + t);
+
+	for (t = 0; t < n; t++)
+		*(dest + t) = '\0';
+	return (dest);
+}
