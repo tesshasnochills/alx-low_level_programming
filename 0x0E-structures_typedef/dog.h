088a4c8ade085d28685399fd6612b92452c14e39
tesshasnochills <tess.nganga2020@gmail.com>
2023-01-25 11:46:57 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,13 +13,7 @@ typedef struct dog
 		float age;
 		char *owner;
 		
-} dog_t;
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+} my_dog;
 
 
 #endif /* DOG_H */
