8e5d0b330cefda8d415b0b8d5668bc07144f6148
tesshasnochills <tess.nganga2020@gmail.com>
2023-01-30 23:05:45 +0300

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* print_list - prints elements in a linked list
+* @h: pointer to head first node
+*
+* Return: the number of nodes in list
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("%s", ("[0] (nil)\n"));
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
