9b2cfdd184dcf584a00b5646ffc0d3712e723127
tesshasnochills <tess.nganga2020@gmail.com>
2023-02-02 21:29:36 +0300

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,6 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newNode;
 		return (newNode);
 	}
+
 	for (x = 0; x < idx - 1 && temp; x++)
 		temp = temp->next;
 
