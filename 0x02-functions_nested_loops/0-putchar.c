cfdceaf5a1f2892300d723ee1f6e48d051e4d873
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-19 17:13:59 +0300

    Learn your times table

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,21 @@
 
 /**
  * main - file entry point
- *
+ * 
+ * FUNCTIONALITY *
+ * 1. Character t is initialised to point to the string
+ * 
+ * 2. A while loop is entered which continues as long as char pointed by t != '\0'
+ * 
+ * 3. The loop uses putchar to print the character pointed to by t
+ * 
+ * 4. T is incremented by 1 so as to point to the next character in the string.
+ * 
+ * 5. The loop contiues until the null character is reached
+ * 
+ * 6. Once the loop ends a new line is printed and return 0 signifies successful program execution.
+ * 
+ * 
  * Return: Always 0 (Success)
  */
 
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,17 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase.
  *
+ * 
+ * FUNCTIONALITY *
+ * 
+ * 1. Variable t is declared as a character.
+ * 
+ * 2. The for loop iterates form a to z as t has been equated to a.
+ * 
+ * 3. T is then printed.
+ * 
+ * 4. Once the loop ends a new line is printed.
+ * 
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
