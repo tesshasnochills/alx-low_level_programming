728594a0b39e50993a9700a1de18f3a79a384789
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-19 13:47:28 +0300

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,11 +3,24 @@
 /**
  * main - prints all possible combinations of two digits.
  * 
- * Return: Always 0 (Success)
  * 
  * FUNCTIONALITY *
+ * 1. Variables i and j are initialized to 0
+ * 2. A while loop is used to iterate through the values,
+ *      of i and j from 0 to 9
+ * 3. A nested if loop checks if i is less than j
+ * 4. If it is i and j are printed as their corresponding character values
+ * 5. Another nested if loop checks is i=8 and j=9,
+ *      If true do nothing
+ * 6. if not, print a comma followed by a space
+ * 7. J is incremented by one
+ * 8. Steps 3-7 are repeated until j<=9 becomes false
+ * 9. After that nested loop ends, j is set to 0,
+ *      and incremented by one.
+ * 10. A new line is printed and O is returned,
+ *      signifying successful execution.
  * 
- * 
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -15,7 +28,7 @@ int main(void)
 
     while (i <= 9)
     {
-        while (j =< 9)
+        while (j <= 9)
         {
             if (i < j)
             {
@@ -24,7 +37,7 @@ int main(void)
 
                 if (i == 8 && j == 9)
                 {
-                    //do nothing
+                    /*do nothing*/
                 }
                 else
                 {
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,12 +3,18 @@
 /**
  * main - prints all possible number combinations of single digit numbers
  *
- * Return: Always 0 (Success)
  * 
  * FUNCTIONALITY *
  * 
  * 	1. Declare an integer variable a and initalize it to 0
- * 	2. 
+ * 	2. A while loop is intialised as long as a is less than 10
+ *  3. if so print a as a character value
+ *  4. If a is not equal to 9, print a comma then space
+ *  5. A is then incremented by 1
+ *  6. Print a new line
+ *  7. Return 0 indicates successful execution
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
