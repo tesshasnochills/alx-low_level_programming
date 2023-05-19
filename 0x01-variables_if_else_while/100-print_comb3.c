6d45f3c9a64f80228913d9f06a3daa74e38d96e9
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-19 13:58:58 +0300

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,16 @@
  *
  * FUNCTONALITY *
  *
- * 1. The outer for loop is used to print the first digit of the two digit number.
- * 2. The inner for loop is used to print the second digit of the two digit number.
- * 3. The if statement is used to print the comma and space after each two digit number except the last two digit number.
- * 4. The putchar() function is used to print the digits and the comma and space.
- * 5. The continue statement is used to skip the printing of the comma and space after the last two digit number.
+ * 1. The outer for loop is used to print the first,
+ *      digit of the two digit number.
+ * 2. The inner for loop is used to print the second digit,
+ *      of the two digit number.
+ * 3. The if statement is used to print the comma and space,
+ *      after each two digit number except the last two digit number.
+ * 4. The putchar() function is used to print the digits and the comma,
+ *       and space.
+ * 5. The continue statement is used to skip the printing,
+ *       of the comma and space after the last two digit number.
  * 6. The putchar('\n') function is used to print a new line after the last two digit number.
  */
 
