da37986b14d23ce465f276b352cf2ef7e087dd11
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-19 16:48:04 +0300

    Code functionality

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,20 @@
 
 /**
  * main - printing all possible combinations of a three digit number
- *
+ * 
+ * FUNCTIONALITY*
+ * 1. int i, j and k are declared as integers
+ * 2. A for loop that iterates over values of i from 0-9
+ * 3. A nested loop that iterates over values of j from 0-9
+ * 4. The second nested for loop that iterates over values of k from 0-9
+ * 5. I,j and k are printed using putchar
+ * 6. The if loops checks if the values of i.j.k are true,
+ * 		and if true the program continues.
+ * 7. A comma and space are printed next using putchar if step 6 is untrue.
+ * 8. The loops continue until the condition is false (<=9)
+ * 9. A new line is printed and return 0 signifies successful program execution.
+ * 
+ * 
  * Return: Always 0 (success)
  */
 
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,6 +2,21 @@
 
 /**
  * main - entry point
+ * 
+ *  FUNCTIONALITY *
+ * 1. Integers i and j are declared.
+ * 
+ * 2. The outermost for loop initializes i to zero and iterates values 0-99
+ * 
+ * 3. The nested for loop does the step to but for j
+ * 
+ * 4. Next it checks the values of i and j and prints them
+ * 
+ * 5. The if statement then checks if the values of i and j are 98 and 99 respectively
+ * 
+ * 6. It then prints a comma and a space if condition in 5 isnt satisfied.
+ * 
+ * 7. A new line is printed and return 0 signifies successful program execution
  *
  * Return: Always success (0)
 */
