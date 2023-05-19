09ffb8345eb52fae2d2b98f525dea52273674976
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-19 19:19:07 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,30 +11,26 @@ int main(void)
 {
 	int i;
 
-for (i = 0; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf(" Fizz");
-}
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf(" Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf(" FizzBuzz");
-}
-else if (i == 0)
-{
-printf("%d", i);
-}
-else
-{
-printf("%d", i);
-}
-}
-printf("\n");
-
-return (0);
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
 }
