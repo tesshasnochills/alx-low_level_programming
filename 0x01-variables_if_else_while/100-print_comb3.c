4236d02f3b5f4ba95db7c38eee5bb97f4387135d
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-19 14:21:20 +0300

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints all combinations of two digits with,
  * and space followed by new line
@@ -16,9 +17,9 @@
  *       and space.
  * 5. The continue statement is used to skip the printing,
  *       of the comma and space after the last two digit number.
- * 6. The putchar('\n') function is used to print,
- *      a new line after the last two digit number.
- * 
+ * 6. The putchar('\n') function is used to print
+ *		a new line after the last two digit number.
+ *
  * Return: returns 0
  */
 
