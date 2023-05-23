435b3cc1f01393be6babdc764568faa12bb74896
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-23 17:39:32 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
-* _strcat - concentrates two strings
-* @dest: string to check
-* @src: string to check
-*
-* Return: @dest
-*/
+	* _strcat - concatenates two strings
+	* @dest: string to check
+	* @src: string to check
+	*
+	* Return: @dest
+	*/
 char *_strcat(char *dest, char *src)
 {
 	int s, t;
 
-		for (s = 0; *(dest + s) != '\0'; s++)
-		{}
+	for (s = 0; *(dest + s) != '\0'; s++)
+	{}
 
-		for (t = 0; *(src + t) != '\0'; t++, s++)
-		{
-			*(dest + s) = *(src + t);
-		}
+	for (t = 0; *(src + t) != '\0'; t++, s++)
+	{
+		*(dest + s) = *(src + t);
+	}
 
-		*(dest + s) = '\0';
+	*(dest + s) = '\0';
 
-		return (dest);
+	return (dest);
 }
