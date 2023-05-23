57372a58aea55cfe6b1a2e2375e305e10af6bd94
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-23 18:09:25 +0300

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
-* cap_string - capitalixes all words of a string
-* @t: string to check
+* cap_string - capitalizes first letter of every word
+* @s:  string to check
 *
-* Return: pointer to the sring
+* Return: pointer to string
 */
-char *cap_string(char *t)
+char *cap_string(char *s)
 {
 	int i, j;
-	char *sep = "\t\n,;.?!\"(){}";
+	char *sep = " \t\n,;.!?\"(){}";
 
-	for (i = 0; t[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && (t[i] >= 'a') && (t[i] <= 'z'))
-			t[i] = t[i] - 32;
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] = s[i] - 32;
 
 		for (j = 0; sep[j] != '\0'; j++)
 		{
-			if ((t[i] == sep[j] && (t[i + 1] >= 'a' && t[i + 1] <= 'z')))
-				t[i + 1] = t[i + 1] - 32;
+			if ((s[i] == sep[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z')))
+				s[i + 1] = s[i + 1] - 32;
 		}
 	}
-	return (t);
+
+	return (s);
 }
